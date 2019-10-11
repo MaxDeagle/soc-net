590d0e78cfb062283b922b5ac42e3bd98720596c
Shlangel <adios_rst@mail.ru>
2019-10-11 14:53:40 +0300

    Global changes

--- soc-network/server/app/routes/index.js
+++ soc-network/server/app/routes/index.js
@@ -1,4 +1,6 @@
-const noteRoutes = require('./note_routes');
+const dialogueRoutes = require('./Dialogue');
+const usersRoutes = require('./Users');
 module.exports = function(app, db) {
-    noteRoutes(app, db);
+    dialogueRoutes(app, db);
+    usersRoutes(app, db);
 }
