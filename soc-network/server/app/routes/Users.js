6904ad299373404f86be60e307fd6bec34d26d86
Shlangel <adios_rst@mail.ru>
2019-10-11 16:16:43 +0300

    Update

--- soc-network/server/app/routes/Users.js
+++ soc-network/server/app/routes/Users.js
@@ -91,7 +91,7 @@ module.exports = function (app, db) {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
       } else {
-        res.send();
+        res.send(result);
       }
     });
   });
